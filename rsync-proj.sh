e3e6a8a4b693550ba809c92eaecda4b2e63027e5
Xiaokun <xkun.wu@gmail.com>
2018-09-06 18:26:55 +0100

    basic test

--- rsync-proj.sh
+++ rsync-proj.sh
@@ -11,10 +11,17 @@ TARGET=${SERVER}:${PROJ_DIR}/${PROJ_NAME}/
 echo uploading \
     from: [${SOURCE}] \
     to: [${TARGET}]
-rsync -auvh -e ssh \
+sshpass -e rsync -auvh -e ssh \
     --exclude-from='.gitignore' \
     ${SOURCE} \
     ${TARGET}
+
+## run test scrip
+# read -p "Press enter to runc test script ..."
+# sshpass -e ssh ${SERVER} ". .profile; . .bashrc; ${PROJ_DIR}/${PROJ_NAME}/run_test.sh"
+# sshpass -e ssh ${SERVER} "echo '$PATH' && source '$HOME/.bashrc' && echo '$PATH' && which python"
+# sshpass -e ssh ${SERVER} "bash -c 'export PATH=$HOME/anaconda3/bin:$PATH' && echo '$PATH' && which python"
+# sshpass -e ssh ${SERVER} -t "cd '${PROJ_DIR}/${PROJ_NAME}' && echo '$PATH'"
 
 # ## download predictions
 # SOURCE=${SERVER}:${OUT_DIR}/output/${DATA_NAME}/predict/
